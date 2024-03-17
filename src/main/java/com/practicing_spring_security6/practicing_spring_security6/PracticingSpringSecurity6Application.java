9db8cd409f5678ee366f43bd28a95a20c5855cfa
rodrigoDev <rodriguinho2456@gmail.com>
2024-03-17 10:06:25 -0300

    initial project

---
+++ src/main/java/com/practicing_spring_security6/practicing_spring_security6/PracticingSpringSecurity6Application.java
@@ -0,0 +1,13 @@
+package com.practicing_spring_security6.practicing_spring_security6;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PracticingSpringSecurity6Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PracticingSpringSecurity6Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/practicing_spring_security6/practicing_spring_security6/PracticingSpringSecurity6ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.practicing_spring_security6.practicing_spring_security6;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PracticingSpringSecurity6ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
